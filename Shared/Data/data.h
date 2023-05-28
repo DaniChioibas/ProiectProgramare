4850415f609752df19fe8adbdc8bbb7b2fb95110
DaniChioibas <chioibas.dani@gmail.com>
2023-05-28 16:34:00 +0300

    Dani Foldere+Clase(data,ora)

---
+++ Shared/Data/data.h
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Data
+{
+    int zi;
+    int luna;
+    int an;
+    string numeZi;
+
+public:
+    Data();
+    Data(int, int, int, string);
+    int getZi();
+    int getLuna();
+    int getAn();
+    string getNumeZi();
+    void setZi(int);
+    void setLuna(int);
+    void setAn(int);
+    void setNumeZi(string);
+    friend ostream &operator<<(ostream &, const Data &);
+};
+Data::Data()
+{
+    this->zi = 0;
+    this->luna = 0;
+    this->an = 0;
+    this->numeZi = "";
+}
+Data::Data(int zi, int luna, int an, string numeZi)
+{
+    this->zi = zi;
+    this->luna = luna;
+    this->an = an;
+    this->numeZi = numeZi;
+}
+int Data::getAn()
+{
+    return this->an;
+}
+int Data::getLuna()
+{
+    return this->luna;
+}
+int Data::getZi()
+{
+    return this->zi;
+}
+string Data::getNumeZi()
+{
+    return this->numeZi;
+}
+void Data::setZi(int x)
+{
+    this->zi = x;
+}
+void Data::setLuna(int x)
+{
+    this->luna = x;
+}
+void Data::setAn(int x)
+{
+    this->an = x;
+}
+void Data::setNumeZi(string x)
+{
+    this->numeZi = x;
+}
+ostream &operator<<(ostream &out, const Data &x)
+{
+    out << x.zi << "." << x.luna << "." << x.an << " " << x.numeZi;
+    return out;
+}
---
+++ Shared/Data/ora.h
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class Ora
+{
+    int ora;
+    int minute;
+
+public:
+    Ora();
+    Ora(int, int);
+    int getOra();
+    int getMinute();
+    void setMinute(int x);
+    void setOra(int x);
+    friend ostream &operator<<(ostream &, const Ora &);
+};
+
+Ora::Ora()
+{
+    this->ora = 0;
+    this->minute = 0;
+}
+Ora::Ora(int ora, int minute)
+{
+    this->ora = ora;
+    this->minute = minute;
+}
+int Ora::getOra()
+{
+    return this->ora;
+}
+int Ora::getMinute()
+{
+    return this->minute;
+}
+void Ora::setMinute(int x)
+{
+    this->minute = x;
+}
+void Ora::setOra(int x)
+{
+    this->ora = x;
+}
+ostream &operator<<(ostream &out, const Ora &x)
+{
+    out << x.ora << ":" << x.minute;
+    return out;
+}
