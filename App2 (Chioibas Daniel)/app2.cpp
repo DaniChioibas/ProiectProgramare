1d7a75fd746b5247baa8132fd76b81deceba75e7
Dani Chioibas <chioibas.dani@gmail.com>
2023-05-30 16:07:12 +0300

    app2

--- App2 (Chioibas Daniel)/app2.cpp
+++ App2 (Chioibas Daniel)/app2.cpp
@@ -8,26 +8,27 @@
 #include "../Shared/Data/programare.h"
 using namespace std;
 void optiuni();
-void catalog_oferte();
-void programari_toate();
+void catalog_oferte(int, char *[]);
+void programari_toate(int, char *[]);
+void programare_nou(int, char *[]);
+void programari(int, char *[]);
 int main(int argc, char *argv[])
 {
     if (argc <= 1)
         optiuni();
-    else if (argc == 2)
+    else
     {
-        string optiune = argv[1];
-        if (optiune == "catalog_oferte")
-            catalog_oferte();
-        else if (optiune == "programari_toate")
-            programari_toate();
+        string x = argv[1];
+        if (x == "programari")
+            programari(argc, argv);
+        else if (x == "programari_toate")
+            programari_toate(argc, argv);
+        else if (x == "programare_nou")
+            programare_nou(argc, argv);
+        else if (x == "catalog_oferte")
+            catalog_oferte(argc, argv);
         else
             optiuni();
-    }
-    else if (argc == 3)
-    {
-        // data x.y.z
-        string data = argv[2];
     }
 }
 
@@ -39,12 +40,43 @@ void optiuni()
     cout << "   programari_toate\n";
     cout << "   programare_nou <data> <ora> <numePersoana> <oferta>";
 }
-void catalog_oferte()
+void programare_nou(int argc, char *argv[])
 {
+    if (argc != 6)
+    {
+        cout << "Argument invalid. Foloseste ./app2 programare_nou <data> <ora> <numePersoana> <oferta>";
+        cout << "<data>: ab.cd.abcd\n";
+        cout << "<ora>: ab:ab\n";
+        cout << "<numePersoana> \"Ex Nume\"\n";
+        cout << "<oferta>: idOferta ";
+        return;
+    }
+}
+void programari(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        cout << "Argument invalid. Foloseste ./app2 programari <data>";
+        cout << " Ex <data>";
+        return;
+    }
+}
+void catalog_oferte(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        cout << "Argument invalid. Foloseste ./app2 catalog_oferte";
+        return;
+    }
     cout << "Acesta este catalogul cu oferte: ";
 }
-void programari_toate()
+void programari_toate(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        cout << "Argument invalid. Foloseste ./app2 programari_toate";
+        return;
+    }
     ifstream input;
     cout << "Acestea sunt toate programarile: \n";
     input.open("../Shared/Files/programari.txt");
